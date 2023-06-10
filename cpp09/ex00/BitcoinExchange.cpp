ff4a1dd68e1c43e158a21cb9b705c24a2229fd42
Eyup kemal Karaoglu <ekaraogl@k2m05s07.42kocaeli.com.tr>
2023-06-10 17:36:53 +0300

    ex00 finish

---
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,46 @@
+
+#include "BitcoinExchange.hpp"
+
+void    BitcoinExchange::readdata() {
+    std::ifstream file("data.csv");
+    if (file.is_open()) {
+            std::string line;
+
+            while (std::getline(file, line)) {
+                std::stringstream ss(line);
+                std::string key;
+                double value;
+
+                if (std::getline(ss, key, ',') && ss >> value) {
+                    data[key] = value;
+                }
+            }
+            file.close();
+    }
+}
+
+
+double BitcoinExchange::give_back_data(std::string key, double value) {
+    if (key < data.begin()->first)
+        return (0);
+    else {
+        std::map<std::string, double>::iterator prev = data.begin();
+        std::map<std::string, double>::iterator it;
+        for (it = data.begin(); it != data.end(); ++it) {
+            if (it->first >= key) {
+                if (it->first == key)
+                    return (value * it->second);
+                else
+                    return (value * prev->second);
+            }
+            prev = it;
+        }
+    }
+    return (1);
+}
+
+void    BitcoinExchange::print(void) {
+    for (std::map<std::string, double>::iterator it = this->data.begin(); it != this->data.end(); it++) {
+        std::cout << it->first << " " << it->second << std::endl;
+    }
+}
---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <map>
+
+class BitcoinExchange {
+    public:
+        std::map<std::string, double>   data;
+        std::map<std::string, double>   exchange;
+
+        void readdata();
+        double give_back_data(std::string key, double value);
+        void print();
+
+        class FileNotOpen : public std::exception {
+            public:
+                const  char *what() const throw(){
+                        return "file can not open .";
+                }
+        };
+};
+
